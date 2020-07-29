6f16e2cd5602b96c045c57a520c95d12845cbbbe
FilippK <filippkotenko@outlook.com>
2020-07-29 19:43:46 +0300

    newComponent

--- src/App.js
+++ src/App.js
@@ -1,67 +1,18 @@
 import React from "react";
-import "./styles.css";
+import "./css/styles.css";
 import Notes from "./components/Notes";
-import { useState } from "react";
-import Sublist from "./components/Sublist";
-import UUID from "pure-uuid";
-
-function App() {
-
-  let [notes, setNotes] = useState([]);
+import uuidv4 from "./components/uuid";
+import ListsEl from "./components/ListsEl";
 
 
-  let listsEl = notes.map(n => (
-    <ul key={n.id}>
-      <input type="hidden" checkbox={n.isDone} />
-      <span>{n.title}</span>
-      <button className="remove" onClick={() => notesDel(n.id)}>
-        remove
-        <button onClick={() => props.sublist}>addSublist</button>
-      </button>
-    </ul>
-  ));
 
-  let notesDel = id => {
-    let notesDelEl = notes.filter(n => n.id !== id);
-    setNotes(notesDelEl);
-  };
-  let notesAdd = text => {
-    ;
-    let notesAddEl = { id: uuidv4(), title: text };
-    let notesAddEls = [notesAddEl, ...notes];
-    setNotes(notesAddEls);
-  };
-  function uuidv4() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-      var r = (Math.random() * 16) | 0,
-        v = c == "x" ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
-  }
-  // let plusNotes = id => {
-  //   let plusNotesEl = notes.sort();
 
-  //   let changePlusEl = [plusNotesEl, ...notes];
-  //   setNotes(changePlusEl);
-  // };
-
+const App = () => {
   return (
     <div>
-      <Notes
-        title="What to notes"
-        notes={notes}
-        listsEl={listsEl}
-        notesDel={notesDel}
-        notesAdd={notesAdd}
-        plusScroll={plusScroll}
-      />
+      <ListsEl />
     </div>
   );
 }
 
 export default App;
-//removeNotes={removeNotes}
-//  let removeNotes = (id) => {
-//  let removeEl = notes.filter(n => n.id != id);
-//   setNotes(removeEl)
-// }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,48 +1,38 @@
-import React, { useState } from "react";
-import "../styles.css";
-import Sublist from "./Sublist";
+import React, {useState} from "react";
+import "../css/styles.css";
 
 const Notes = props => {
-    const sublist = () => {
-        return (
-            <div>
-                <Sublist/>
+
+    const [newNotes, setNotes] = useState(" ");
+
+    return (
+        <div className="notes">
+            <div className="input">
+                <h3>{props.title}</h3>
             </div>
-        )
-    }
-  const [newNotes, setNotes] = useState(" ");
-  return (
-    <div className="notes">
-      <div className="input">
-        <h3>{props.title}</h3>
-      </div>
-      <div>{props.listsEl}</div>
-      <div>
-        <button onClick={() => props.plusScroll}>+</button>
-        <button>-</button>
-        <input
-          onKeyPress={e => {
-            if (e.charCode === 13) {
-              props.notesAdd(newNotes);
-              setNotes("");
-            }
-          }}
-          value={newNotes}
-          onChange={e => {
-            setNotes(e.currentTarget.value);
-          }}
-        />
-        <button
-          onClick={e => {
-            props.notesAdd(newNotes);
-            setNotes("");
-          }}
-        >
-          Add
-        </button>
-      </div>
-    </div>
-  );
+            <div>{props.listsEl}</div>
+            <div>
+                <button onClick={props.plusScroll}>+</button>
+                <button onClick={props.minusScroll}>-</button>
+                <input
+                    onKeyPress={e => {
+                        if (e.charCode === 13) {
+                            props.notesAdd(newNotes);
+                            setNotes("");
+                        }
+                    }}
+                    value={newNotes}
+                    onChange={e => {
+                        setNotes(e.currentTarget.value);
+                    }}
+                />
+                <button
+                    onClick={e => {
+                        props.notesAdd(newNotes);
+                        setNotes("");}}>Add</button>
+            </div>
+        </div>
+    );
 };
 
 export default Notes;
